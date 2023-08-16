39ab855c5d8190721342bef05417989447cd5663
Devam Doshi <devamdoshi212@gmail.com>
2023-08-16 13:54:47 +0530

    add get request, spinner through npm, bootstrap link

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React from 'react';
-
-import MoviesList from './components/MoviesList';
-import './App.css';
+import React, { useState } from "react";
+import { RingLoader } from "react-spinners";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [loading, setloading] = useState(false);
+  const fetchMoviesHandler = async () => {
+    setloading(true);
+    const Response = await fetch("https://swapi.dev/api/films");
+    const data = await Response.json();
+    const tranformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        releaseDate: movie.release_date,
+        openingText: movie.opening_crawl,
+      };
+    });
+    setMovies(tranformedMovies);
+    setloading(false);
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!loading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!loading && movies.length === 0 && <p>No Movies Found</p>}
+        {loading && (
+          <div className="d-flex justify-content-center">
+            <RingLoader color={"#000000"}></RingLoader>
+          </div>
+        )}
       </section>
     </React.Fragment>
   );
